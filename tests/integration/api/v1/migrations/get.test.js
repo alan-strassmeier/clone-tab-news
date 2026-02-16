15f0ab44c71396c1ae16e170f4cf5e85ee42e06c
Alan <alanstrassmeier@gmail.com>
2026-02-16 16:46:10 -0300

    test: rename all descriptions to use new pattern

--- tests/integration/api/v1/migrations/get.test.js
+++ tests/integration/api/v1/migrations/get.test.js
@@ -6,13 +6,16 @@ beforeAll(async () => {
   await database.query("drop schema public cascade; create schema public;");
 });
 
-test("GET to api/v1/migrations returns status 200 and correct body", async () => {
-  const response = await fetch("http://localhost:3000/api/v1/migrations");
-  expect(response.status).toBe(200);
+describe("GET api/v1/migrations", () => {
+  describe("Anonymous user", () => {
+    test("Retriving pending migrations", async () => {
+      const response = await fetch("http://localhost:3000/api/v1/migrations");
+      expect(response.status).toBe(200);
 
-  const responseBody = await response.json();
-  console.log(responseBody);
+      const responseBody = await response.json();
 
-  expect(Array.isArray(responseBody)).toBe(true);
-  expect(responseBody.length).toBeGreaterThanOrEqual(0);
+      expect(Array.isArray(responseBody)).toBe(true);
+      expect(responseBody.length).toBeGreaterThanOrEqual(0);
+    });
+  });
 });
