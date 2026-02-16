1304a373f55a44478546edc1bf01cf24f87b3a66
Alan <alanstrassmeier@gmail.com>
2026-02-16 20:19:04 -0300

    style: fix prettier formatting on tests scripts

--- tests/integration/api/v1/migrations/post.test.js
+++ tests/integration/api/v1/migrations/post.test.js
@@ -9,9 +9,12 @@ describe("POST api/v1/migrations", () => {
   describe("Anonymous user", () => {
     describe("Running pending migrations", () => {
       test("For the first time", async () => {
-        const response = await fetch("http://localhost:3000/api/v1/migrations", {
-          method: "POST",
-        });
+        const response = await fetch(
+          "http://localhost:3000/api/v1/migrations",
+          {
+            method: "POST",
+          },
+        );
         expect(response.status).toBe(201);
 
         const responseBody = await response.json();
@@ -19,17 +22,18 @@ describe("POST api/v1/migrations", () => {
         expect(responseBody.length).toBeGreaterThan(0);
       });
       test("For the second time", async () => {
-        const response = await fetch("http://localhost:3000/api/v1/migrations", {
-          method: "POST",
-        });
+        const response = await fetch(
+          "http://localhost:3000/api/v1/migrations",
+          {
+            method: "POST",
+          },
+        );
         expect(response.status).toBe(200);
 
         const responseBody = await response.json();
         expect(Array.isArray(responseBody)).toBe(true);
         expect(responseBody.length).toBe(0);
       });
-
-    })
-  })
-})
-
+    });
+  });
+});
