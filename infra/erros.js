57ff4647cbe77e976703772f4d135c18caeb20c5
Alan <alanstrassmeier@gmail.com>
2026-02-23 21:34:25 -0300

    feat: create and use `InternalServerError` in `api/v1/status`

---
+++ infra/erros.js
@@ -0,0 +1,18 @@
+export class InternalServerError extends Error {
+  constructor({ cause }) {
+    super("Um erro interno não esperado ocorreu.", { cause }
+    );
+    this.name = "InternalServerError";
+    this.action = "Tente novamente mais tarde. Se o erro persistir, entre em contato com o suporte.";
+    this.statusCode = 500;
+  }
+
+  toJason() {
+    return {
+      name: this.name,
+      message: this.message,
+      action: this.action,
+      status_Code: this.statusCode,
+    }
+  }
+}
